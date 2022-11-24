08de8f7f8da2bbbfcefa3844adad3acc77d9a326
gabrielbigliardi <gabrielbigliardi@gmail.com>
2022-11-24 01:28:26 -0300

    trying to show images

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -3,13 +3,34 @@ import './App.css'
 import memesData from './memesData'
 
 export default function Meme(){
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        // const memeUrl = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
     return (
         <main className='meme'>
             <div className='meme--form'>
-                <input className='meme--input' placeholder='top text' type="text" />
-                <input className='meme--input' placeholder='bottom text' type="text" />
-                <button className='meme--button'>Get a new meme image  🖼</button>
-                <img href={memesData.data.memes[0]}></img>
+                <input 
+                    className='meme--input' 
+                    placeholder='top text' 
+                    type="text" 
+                />
+                <input 
+                    className='meme--input' 
+                    placeholder='bottom text' 
+                    type="text" />
+                <button 
+                    className='meme--button'
+                >
+                    Get a new meme image  🖼
+                </button>
+                <img className='meme--image' src={memeImage}></img>
             </div>
         </main>
     )
